7b9a0a6330171d41aa5c92ee0a582723579e4f63
unknown <fmreda3@gmail.com>
2020-05-29 16:17:04 +0000

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul className="nav nav-tabs" id="navId">
+          <li className="nav-item">
+            <a href="#tab1Id" className="nav-link active">
+              Home
+            </a>
+          </li>
+
+          <li className="nav-item">
+            <a href="#tab5Id" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <p>Feed TwitteR</p>
       </header>
+      <blockquote class="twitter-tweet" data-partner="tweetdeck">
+        <p lang="en" dir="ltr">
+          hello
+        </p>
+        &mdash; 3wa developer (@3waDeveloper){" "}
+        <a href="https://twitter.com/3waDeveloper/status/1266395312341561345?ref_src=twsrc%5Etfw">
+          May 29, 2020
+        </a>
+      </blockquote>
+      <a class="twitter-share-button" href="https://twitter.com/intent/tweet">
+        Tweet
+      </a>
     </div>
   );
 }
