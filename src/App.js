3ca2d1c704e93001ae3f1335d8532e11fddb2e11
unknown <fmreda3@gmail.com>
2020-05-30 20:52:05 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+// import Search from "./Search";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App container">
-      <header className="App-header">
-        <ul className="nav nav-tabs" id="navId">
-          <li className="nav-item">
-            <a href="#tab1Id" className="nav-link active">
-              Home
-            </a>
-          </li>
-
-          <li className="nav-item">
-            <a href="#tab5Id" className="nav-link">
-              Contact
-            </a>
-          </li>
-        </ul>
-        <p>Feed TwitteR</p>
-      </header>
-      <blockquote class="twitter-tweet" data-partner="tweetdeck">
-        <p lang="en" dir="ltr">
-          hello
-        </p>
-        &mdash; 3wa developer (@3waDeveloper){" "}
-        <a href="https://twitter.com/3waDeveloper/status/1266395312341561345?ref_src=twsrc%5Etfw">
-          May 29, 2020
-        </a>
-      </blockquote>
-      <a class="twitter-share-button" href="https://twitter.com/intent/tweet">
-        Tweet
-      </a>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="App container mt-5">
+          <Main />
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/main/Search.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+// import Parser from "html-react-parser";
+import axios from "axios";
+
+// import Navbar from './Navbar';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton,
+} from "react-twitter-embed";
+export class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tweet: [],
+      search: "",
+      result_type: "mixed",
+      count: 1,
+    };
+  }
+
+  gettwitte = (search, count) => {
+    console.log(search);
+    if (search == "") {
+      alert("merci de mettre un mot pour la recherche");
+    } else {
+      const api = `https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/search/tweets.json?q=${search}&result_type=mixed&count=${count}`;
+      const token = `AAAAAAAAAAAAAAAAAAAAAAs5%2FAAAAAAA%2BFhxtLDRr2AuKh5zdIHTczhg0Jg%3DltF0dqGzLFlmXH9wjI8HkO1gEzGlnCYUegwIOVVu1Umn8Yi1sX`;
+      axios
+        .get(api, { headers: { Authorization: `Bearer ${token}` } })
+        .then((res) => {
+          this.setState({
+            tweet: res.data.statuses,
+          });
+        });
+    }
+  };
+  handleChange = (e) => {
+    // console.log(e.target.value);
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // console.log(e.target[0].value);
+    this.setState({ search: e.target[0].value });
+    // console.log(e.target[1].value);
+    this.setState({ count: e.target[1].value });
+  };
+  componentDidUpdate() {
+    // console.log(this.state.search);
+    this.gettwitte(this.state.search, this.state.count);
+  }
+  componentDidMount() {}
+
+  // Git=(data)=>{
+
+  //     const api = `https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/search/tweets.json?q=%23${data}&result_type=recent`;
+  //     const token = `AAAAAAAAAAAAAAAAAAAAAAs5%2FAAAAAAA%2BFhxtLDRr2AuKh5zdIHTczhg0Jg%3DltF0dqGzLFlmXH9wjI8HkO1gEzGlnCYUegwIOVVu1Umn8Yi1sX`;
+  //     axios.get(api, { headers: {"Authorization" : `Bearer ${token}`} }).then(res => {
+  //        console.log(res.data.statuses)
+  //        this.setState({
+  //         tweet:res.data.statuses
+  //        })
+  //     })
+
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>Search component</h1>
+        <form
+          onSubmit={this.handleSubmit}
+          className="form-inline my-2 my-lg-0 mt-5"
+        >
+          <input
+            className="form-control mr-sm-2"
+            type="text"
+            placeholder="Search"
+            onChange={this.handleChange}
+          />
+          <select className="mdb-select md-form mr-2">
+            <option value={this.state.count} disabled selected>
+              Number of tweets
+            </option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+          </select>
+          <button className="btn btn-primary my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+        {/* <Navbar  /> */}
+        {/* <Search twitterSearch={this.Git} /> */}
+        <div className="container">
+          <div className="row mt-5">
+            {this.state.tweet.map((tw) => (
+              <div className="col-md-4 mt-4" key={tw.id_str}>
+                <TwitterTweetEmbed tweetId={tw.id_str} />
+
+                <a href="" className="btn btn-danger btn-blocl">
+                  Ajouter au favoris
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
